2f720c7deba421048fb52d8e6c24671a75c6fdbd
Michael Froehlich <m.froehlich@tum.de>
2018-06-30 18:45:30 +0200

    Pod install siren

--- Pods/Target Support Files/Pods-ping/Pods-ping-frameworks.sh
+++ Pods/Target Support Files/Pods-ping/Pods-ping-frameworks.sh
@@ -145,10 +145,12 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Appirater/Appirater.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/GBPing/GBPing.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Siren/Siren.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Appirater/Appirater.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/GBPing/GBPing.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Siren/Siren.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
